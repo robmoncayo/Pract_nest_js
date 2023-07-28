789ebf59d3b5f4eb95b7335f1adf7a0d30c9646d
robmoncayo <rmoncayoandrade@gmail.com>
2023-07-28 11:54:25 -0500

    Desde aqui todo en orden 1.0.1

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,15 @@
-export class CreateItemDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateItemDto {
+
+@IsNotEmpty()
+
+    @IsNotEmpty()
+    name: string;
+    @IsNotEmpty()
+    price: number;
+    @IsNotEmpty()
+    description: string;
+
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('documentation', app, document);
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
 bootstrap();
